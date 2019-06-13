310e4f1749c5ec606cc5403cbcc52f81b04f76ff
wdeo3601 <wen974826191@qq.com>
2019-06-13 18:34:52 +0800

    feat : detail layout

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
+import {Content, DetailWrapper, Header} from "./style";
 
 class Detail extends Component {
   render() {
     return (
-      <div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        Detail-
-      </div>
+      <DetailWrapper>
+        <Header>
+12星座排名第一的是什么？看看你是什么？
+        </Header>
+        <Content>
+          <img src='https://upload-images.jianshu.io/upload_images/5462150-050f36b225d62998?imageMogr2/auto-orient/strip%7CimageView2/2/w/599/format/webp'/>
+          <p>其实是他主动跟女孩提出的分手，可他却非常痛苦。因为他发现自己好像比之前更爱女友了。</p>
+          <p>其实是他主动跟女孩提出的分手，可他却非常痛苦。因为他发现自己好像比之前更爱女友了。</p>
+          <p>其实是他主动跟女孩提出的分手，可他却非常痛苦。因为他发现自己好像比之前更爱女友了。</p>
+          <p>其实是他主动跟女孩提出的分手，可他却非常痛苦。因为他发现自己好像比之前更爱女友了。</p>
+          <p>其实是他主动跟女孩提出的分手，可他却非常痛苦。因为他发现自己好像比之前更爱女友了。</p>
+        </Content>
+      </DetailWrapper>
     );
   }
 }
